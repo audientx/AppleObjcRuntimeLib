55e311ad665dd0820b6d6c1d4d43ddd3c36239b4
audientx <audientx@163.com>
2017-09-19 19:50:10 +0800

    add debug

---
+++ DebugRuntime/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  DebugRuntime
+//
+//  Created by Audient Xie on 2017/9/19.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
